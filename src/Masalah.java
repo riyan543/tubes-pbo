9fb0125d301132c8da7c39d41b6db31a0f3f504c
riyan543 <riyanpym04@gmail.com>
2024-12-21 02:48:53 +0700

    Add files via upload

---
+++ src/Masalah.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author User
+ */
+public class Masalah {
+    private String jenisMasalah;
+    private String deskripsi;
+    private String solusi;
+
+    // Constructor
+    public Masalah(String jenisMasalah, String deskripsi, String solusi) {
+        this.jenisMasalah = jenisMasalah;
+        this.deskripsi = deskripsi;
+        this.solusi = solusi;
+    }
+}
