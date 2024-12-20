394bf0701492ec03952ed0f8b5c58c3a325f2f3b
riyan543 <riyanpym04@gmail.com>
2024-12-21 03:02:51 +0700

    Add files via upload

---
+++ tubespbo/src/Siswa.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author User
+ */
+public class Siswa {
+     private String nama;
+    private String nis;
+    private String kelas;
+
+    // Constructor
+    public Siswa(String nama, String nis, String kelas) {
+        this.nama = nama;
+        this.nis = nis;
+        this.kelas = kelas;
+    }
+}
