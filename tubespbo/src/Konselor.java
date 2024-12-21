6b367bd8a8d0581e1dbb491a697e8f2288d20b9b
Macrophyllaaa <bayuarya7570@gmail.com>
2024-12-21 12:08:25 +0700

    Add files via upload

---
+++ tubespbo/src/Konselor.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author User
+ */
+public class Konselor {
+     private String nama;
+    private String nip;
+    private String spesialisasi;
+
+    
+    public Konselor(String nama, String nip, String spesialisasi) {
+        this.nama = nama;
+        this.nip = nip;
+        this.spesialisasi = spesialisasi;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String getNip() {
+        return nip;
+    }
+
+    public String getSpesialisasi() {
+        return spesialisasi;
+    }
+    
+}
---
+++ tubespbo/src/Siswa.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author User
+ */
+public class Siswa {
+     private String nama;
+    private String nis;
+    private String kelas;
+
+    // Constructor
+    public Siswa(String nama, String nis, String kelas) {
+        this.nama = nama;
+        this.nis = nis;
+        this.kelas = kelas;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String getNis() {
+        return nis;
+    }
+
+    public String getKelas() {
+        return kelas;
+    }   
+}
